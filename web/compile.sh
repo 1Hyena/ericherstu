1b83babfd20310104162051e1d51a86cf5c34b34
1Hyena <hyena@hyena.net.ee>
2023-06-19 02:45:59 +0300

    Added a kludge to fix the link to CV.

--- web/compile.sh
+++ web/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pdftoppm -jpeg -r 150 ../cv/cv.pdf >gfx/cv.jpg
 ../../mdma/mdma -f framework.html -o index.html content.md
+sed -i 's/href="..\/cv\/cv.pdf"/href="files\/cv.pdf"/' index.html
