e3986891471f266a4068e593491582346924025d
1Hyena <hyena@hyena.net.ee>
2023-07-14 00:03:25 +0300

    Enabled verbose mode for mdma.

--- web/compile.sh
+++ web/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pdftoppm -jpeg -jpegopt progressive=y -r 150 ../cv/cv.pdf >gfx/cv.jpg
-../../mdma/mdma -f framework.html -o index.html content.md
+../../mdma/mdma --verbose -f framework.html -o index.html content.md
 sed -i 's/href="..\/cv\/cv.pdf"/href="files\/cv.pdf"/' index.html
